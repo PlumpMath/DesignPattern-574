d746f80e322f32e4e60ccf5b353758918927505a
James <jh.lastdrop@gmail.com>
2015-10-28 23:28:32 +0900

    Strategy Pattern

--- DesignPatternPrac/src/strategyPattern/Animal.java
+++ DesignPatternPrac/src/strategyPattern/Animal.java
@@ -1,5 +1,37 @@
 package strategyPattern;
 
 public class Animal {
- 
+	String name ;
+	double height ;
+	double weight ;
+	String favFood ;
+	double speed ;
+	String sound ;	
+	
+	public Flys flyingType ;
+	
+	public void setName(String newName) { name = newName ; }
+	public String getName() { return name ; }
+	
+	public void setHeight(double newHeight) { height = newHeight ; }
+	public double getHeight() { return height ; }
+	
+	public double getWeight() { return weight ;}
+	public void setWeight(double newWeight) { weight = newWeight ; }
+	
+	public void setFavFood(String newFavFood) { favFood = newFavFood ; }
+	public String getFavFetood() { return favFood ; }
+	
+	public void setSpeed(double newSpeed) { speed = newSpeed ; }
+	public double getSpeed() { return speed ; }
+	
+	public void setSound(String newSound) { sound = newSound ; }
+	public String getSound() { return sound ; }
+	
+	public String tryToFly() {
+		return flyingType.fly() ;
+	}
+	public void setFlyingAbility(Flys newFlyType) {
+		flyingType = newFlyType ;
+	}
 }
